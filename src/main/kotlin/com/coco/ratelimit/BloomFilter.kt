63bcf7f768cb9c47419bdc89e1f315ebf0bc3a3d
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2024-12-12 00:06:45 +0900

    add bloomFilter
    - 음 해쉬화해서 특정 비트 어레이에 넣는군
    - bloom filter는 true를 반환하는 경우가 있는 이유가 이것 때문이다

---
+++ src/main/kotlin/com/coco/ratelimit/BloomFilter.kt
@@ -0,0 +1,36 @@
+package com.coco.ratelimit
+
+import java.util.*
+import kotlin.math.abs
+
+class BloomFilter(
+    private val bitSet: BitSet,
+    private val size: Int,
+    private val hashSeeds: IntArray
+) {
+
+    fun add(item: String) {
+        for (seed in hashSeeds) {
+            val hash = hash(item, seed)
+            bitSet.set(abs(hash % size))
+        }
+    }
+
+    fun mightContain(item: String): Boolean {
+        for (seed in hashSeeds) {
+            val hash = hash(item, seed)
+            if (!bitSet.get(abs(hash % size))) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun hash(item: String, seed: Int): Int {
+        return item.hashCode() * seed
+    }
+
+    fun reset() {
+        bitSet.clear()
+    }
+}
